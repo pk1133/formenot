24d10252f99be7263ed7bf8a11960c3f74f70080
pk1133 <pk1030@pm.me>
2025-03-04 15:34:53 -0500

    print stuff

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,23 @@ use actix_web::{web, App, HttpServer};
 mod handlers;
 mod models;
 mod db;
-
+println!("Hello, world!");
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    println!("Hello World");
     // Initialize DB pool (this could use Diesel or SQLx)
-    let pool = db::init_pool();
+    // let pool = db::init_pool();
 
-    HttpServer::new(move || {
-        App::new()
-            .data(pool.clone())
-            .service(
-                web::scope("/api")
-                    .service(handlers::get_vendors)
-                    .service(handlers::create_order),
-            )
-    })
-    .bind("0.0.0.0:8080")?
+    // HttpServer::new(move || {
+    //     App::new()
+    //         .data(pool.clone())
+    //         .service(
+    //             web::scope("/api")
+    //                 .service(handlers::get_vendors)
+    //                 .service(handlers::create_order),
+    //         )
+    // })
+    .bind("127.0.0.1:8080")?
     .run()
     .await
 }
